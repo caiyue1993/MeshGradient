9bb1421931a772849626aa0acdfabd4c7716a46f
Yue Cai <yuecai.nju@gmail.com>
2024-10-10 15:04:15 +0800

    p

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MeshGradient",
+    name: "CustomMeshGradient",
 	platforms: [
 		.macOS(.v10_15),
 		.iOS(.v13),
@@ -12,14 +12,14 @@ let package = Package(
 	],
     products: [
         .library(
-            name: "MeshGradient",
+            name: "CustomMeshGradient",
             targets: ["MeshGradient"]),
 		.library(name: "MeshGradientCHeaders",
 				 targets: ["MeshGradientCHeaders"]),
     ],
     targets: [
         .target(
-            name: "MeshGradient",
+            name: "CustomMeshGradient",
             dependencies: ["MeshGradientCHeaders"],
 			resources: [.copy("DummyResources/")]
 		),
